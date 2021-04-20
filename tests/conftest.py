5ad3f2c26509249dc371fa30a48fb7823e008a9d
Allan <psallan300@gmail.com>
2021-04-20 20:08:33 -0300

    Tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,7 +11,7 @@ with open(os.path.join(os.path.dirname(__file__), "data.sql"), "rb") as f:
 
 @pytest.fixture
 def app():
-    db_fd, db_path = tempfile.mkdtemp()
+    db_fd, db_path = tempfile.mkstemp()
 
     app = create_app(
         {
@@ -38,3 +38,21 @@ def client(app):
 @pytest.fixture
 def runner(app):
     return app.test_cli_runner()
+
+
+class AuthActions(object):
+    def __init__(self, client):
+        self._client = client
+
+    def login(self, username="test", password="test"):
+        return self._client.post(
+            "/auth/login", data={"username": username, "password": password}
+        )
+
+    def logout(self):
+        return self._client.get("/auth/logout")
+
+
+@pytest.fixture
+def auth(client):
+    return AuthActions(client)
